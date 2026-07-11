3d15be3e88efeeec7408ef9281f2a405d67f17bf
shadow0392 <shadowz0393@gmail.com>
2026-07-11 23:47:41 +0100

    v0.3.1 Create balance system

---
+++ balance.js
@@ -0,0 +1,14 @@
+/*
+------------------------------------
+PROJECT ODYSSEY
+
+Balance Data
+
+Every number that controls gameplay
+lives in this file.
+
+Changing values here changes the game
+without editing the engine.
+
+------------------------------------
+*/
